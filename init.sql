49f82e9f55c6e39dc9e7f17b94881d6682c1d34d
yegorp <yeagorpereverzev@gmail.com>
2020-10-31 20:26:26 +0300

    fix docker docker-compose and Dockerfile

---
+++ init.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS events (
+        id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+            event_type varchar(255),
+                player1 varchar(255),
+                    player2 varchar(255),
+                        odds1 float,
+                            odds2 float,
+                                status varchar(255),
+                                    time integer,
+                                        flashscore_id varchar(255)
+                                        
+    );
+
+    CREATE TABLE IF NOT EXISTS bets (
+            id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+                user_id integer,
+                    match_id integer,
+                        bet varchar(255),
+                            odds float,
+                                amount integer,
+                                    prize integer
+                                    
+        );
