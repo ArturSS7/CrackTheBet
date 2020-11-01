4f8d280fb3a86e7d999a4d63b1c9ad2b3424fc41
Debych <zh.eliseev@yandex.ru>
2020-11-01 22:55:58 +0300

    finally working DB

--- apiParser/init_db.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-conn = psycopg2.connect(dbname='betdb', user='keker', password='everybodykissmybody', host='localhost')
-cursor = conn.cursor()
-
-cursor.execute("insert into bets (user_id, match_id, bet, odds, amount, prize) values (5,1,'qwe', 2.2, 123,123)")
-cursor.execute('select * from bets;')
-print(cursor.fetchall())
-cursor.close()
-conn.close()
--- apiParser/main.py
+++ apiParser/main.py
@@ -1,4 +1,5 @@
 import requests
+import psycopg2
 from bs4 import BeautifulSoup
 from models.models import Match, League
 
@@ -8,7 +9,7 @@ r = requests.get("https://d.flashscore.com/x/feed/f_1_0_3_en_1", headers=headers
 lis = r.text.split("ZA÷")[1:]
 for item in lis:
 	leagues.append(League(item))
-for league in leagues:
-	print(league.name)
-	for match in league.matches:
-		print(match)
+# for league in leagues:
+# 	print(league.name)
+# 	for match in league.matches:
+# 		print(match)
