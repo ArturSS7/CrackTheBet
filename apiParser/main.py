d8151145252278cc2bae366b857bf60a1226af63
Debych <zh.eliseev@yandex.ru>
2020-11-04 19:46:25 +0300

    done multi db update and socket

--- apiParser/main.py
+++ apiParser/main.py
@@ -8,15 +8,21 @@ from updater.updater import get_status, get_odds, update_db
 import logging
 import socket
 
+manager = multiprocessing.Manager()
+matches = manager.list()
+
+conn_str = "user='keker' host='db' dbname='betdb' password='kek'"
+
+conn = psycopg2.connect(conn_str)
+cur = conn.cursor()
 
 logging.basicConfig(filename='loggg.log', level=logging.INFO)
 logging.info("connected")
 
-conn = None
-cur = None
-conn = psycopg2.connect("user='keker' host='db' dbname='betdb' password='kek'")
-cur = conn.cursor()
-
+for i in update_db(cur, conn):
+	matches.append(i)
+cur.close()
+conn.close()
 
 def status_resp():
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -45,7 +51,7 @@ class Worker(multiprocessing.Process):
 		self._job_queue = job_queue
 
 	def run(self):
-		conn = psycopg2.connect("user='keker' host='db' dbname='betdb' password='kek'")
+		conn = psycopg2.connect(conn_str)
 		cur = conn.cursor()
 		while True:
 			ID = self._job_queue.get()
@@ -58,20 +64,17 @@ class Worker(multiprocessing.Process):
 				#conn.commit()
 			except AttributeError:
 				cur.execute("delete from events where flashscore_id = '{}'".format(ID))
+				matches.remove(ID)
 				#conn.commit()
-
 			conn.commit()
-
-matches = update_db(cur, conn)
 
 jobs = []
 job_queue = multiprocessing.Queue()
 
-for i in range(5):
+for i in range(10):
 	p = Worker(job_queue)
 	jobs.append(p)
 	p.start()
-
 #Start socket
 sock = threading.Thread(target=status_resp, args=())
 sock.start()
@@ -79,25 +82,10 @@ sock.start()
 
 while(True):
 	start_time = time.time()
-
 	for ID in matches:
-		# status, winner = get_status(ID)
-		# try:
-		# 	odds1, odds2 = get_odds(ID)
-		# except AttributeError:
-		# 	cur.execute("delete from events where flashscore_id = '{}'".format(ID))
-		# 	print("no bets for ", ID)
-		# 	conn.commit()
-		# cur.execute("update events set odds1 = (%s), odds2 = (%s), status = (%s), winner = (%s) where flashscore_id = (%s)", (odds1, odds2, status, winner, ID))
-		# conn.commit()
 		job_queue.put(ID)
-		print()
 	while(not job_queue.empty()):
-		#print("not empty")
 		pass
-	conn.commit()
 	logging.info("Update took : {}".format(time.time() - start_time))
-	time.sleep(30)
-
-cur.close()
-conn.close()
+	print("cycle done")
+	time.sleep(10)
